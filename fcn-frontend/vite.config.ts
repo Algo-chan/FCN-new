eb0f88d24a1b34fd4ae6a55fe1ce9648f3879e77
Muhammad Abdulgefar <wealthyempire13@gmail.com>
2026-07-19 16:03:26 +0300

    remove react resolve aliases causing duplicate instances

--- fcn-frontend/vite.config.ts
+++ fcn-frontend/vite.config.ts
@@ -61,9 +61,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "react": path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom")
+      "@": path.resolve(__dirname, "src")
     }
   }
 });
